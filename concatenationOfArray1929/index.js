49728f3488575607ad8cdf051ae9996c8b847b0a
Nima <nima97e@gmail.com>
2022-10-27 21:32:50 +0330

    faster runtime

--- concatenationOfArray1929/index.js
+++ concatenationOfArray1929/index.js
@@ -1,5 +1,14 @@
 var getConcatenation = array=> {
-    array.concat(array)
+    let arr = []
+    for(let i = 0; i <= 1; i++){
+        for(let j = 0; j < array.length; j++){
+            arr.push(array[j])
+        }
+    }
+    return arr
 }
 
 console.log(getConcatenation([1,3,2,1]))
+
+// Runtime: 83 ms, faster than 92.14% of JavaScript online submissions for Concatenation of Array.
+// Memory Usage: 45 MB, less than 73.78% of JavaScript online submissions for Concatenation of Array.
