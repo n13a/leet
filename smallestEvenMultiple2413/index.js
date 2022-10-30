793363ad103f6872dcc00d3c4ec47bed65a49a47
Nima <nima97e@gmail.com>
2022-10-30 20:03:23 +0330

    solved 2413

---
+++ smallestEvenMultiple2413/index.js
@@ -0,0 +1,10 @@
+const smallestEvenMultiple = n => n % 2 !== 0 ? 2 * n : (n / 2) * 2
+
+console.log(smallestEvenMultiple(12))
+
+
+// Runtime: 91 ms
+// Your runtime beats 62.58 % of javascript submissions.
+
+// Memory Usage: 41.5 MB
+// Your memory usage beats 96.64 % of javascript submissions.
