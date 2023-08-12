f2282eeb50270b32f573bf2e13e8aff354c447f6
Nima <nima97e@gmail.com>
2023-08-13 00:25:56 +0330

    remove demo

--- Leet/easy/demo.js
+++
@@ -1,6 +0,0 @@
-const reverseWords = (str) => {
-    let reverseWordArr = str.split(" ").map(word => word.split("").reverse().join(""));
-    return reverseWordArr.join(" ");
-}
-
-console.log(reverseWords('hello, world!'))
