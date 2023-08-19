9325c55f890c962b4cf65191de5832c4058d8915
Nima <nima97e@gmail.com>
2023-08-19 11:30:25 +0330

    Implement Invert binary tree

---
+++ Leet/easy/invertBinaryTree.ts
@@ -0,0 +1,49 @@
+/* 
+Invert Binary Tree
+https://leetcode.com/problems/invert-binary-tree
+
+Given the root of a binary tree, invert the tree, and return its root.
+ 
+Example 1:
+
+Input: root = [4,2,7,1,3,6,9]
+Output: [4,7,2,9,6,3,1]
+
+Example 2:
+
+Input: root = [2,1,3]
+Output: [2,3,1]
+
+Example 3:
+
+Input: root = []
+Output: []
+
+ 
+
+Constraints:
+    The number of nodes in the tree is in the range [0, 100].
+    -100 <= Node.val <= 100
+*/
+
+class TreeNode {
+    val: number
+    left: TreeNode | null
+    right: TreeNode | null
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+        this.val = (val === undefined ? 0 : val)
+        this.left = (left === undefined ? null : left)
+        this.right = (right === undefined ? null : right)
+    }
+}
+
+
+function invertTree(root: TreeNode | null): TreeNode | null {
+    if(root == null){
+        return null
+    }
+    [root.right, root.left] = [root.left, root.right]
+    invertTree(root.right)
+    invertTree(root.left)
+    return root
+};
