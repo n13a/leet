a1d21d4b7f690ffc9f8324963c750db76e3733cd
Nima <nima97e@gmail.com>
2024-08-14 18:52:18 +0330

    Solve Same Tree problem

---
+++ Leet/easy/sameTree.ts
@@ -0,0 +1,45 @@
+/* Same Tree
+https://leetcode.com/problems/same-tree/description/
+
+Given the roots of two binary trees p and q, write a function to check if they are the same or not.
+
+Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
+
+Example 1:
+    Input: p = [1,2,3], q = [1,2,3]
+    Output: true
+
+
+Example 2:
+    Input: p = [1,2], q = [1,null,2]
+    Output: false
+
+Example 3:
+    Input: p = [1,2,1], q = [1,1,2]
+    Output: false
+*/
+
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function isSameTree(p: TreeNode | null, q: TreeNode | null): boolean {
+    if (p === null && q === null) return true;
+
+    if (p === null || q === null) return false;
+
+    if (p.val !== q.val) return false;
+
+    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+};
